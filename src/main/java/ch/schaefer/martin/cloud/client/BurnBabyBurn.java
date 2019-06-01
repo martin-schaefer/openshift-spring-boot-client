7051bc74e86119b21c64b0307b813b8695b16032
Martin Schäfer <martin.schaefer.14@outlook.ch>
2019-06-01 19:54:47 +0200

    activated feign client

--- src/main/java/ch/schaefer/martin/cloud/client/BurnBabyBurn.java
+++ src/main/java/ch/schaefer/martin/cloud/client/BurnBabyBurn.java
@@ -2,6 +2,7 @@ package ch.schaefer.martin.cloud.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * The Lunar Module's main ignition routine.
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Martin Schäfer
  */
 @SpringBootApplication
+@EnableFeignClients
 public class BurnBabyBurn {
 
 	public static void main(String[] args) {
